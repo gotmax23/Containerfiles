gotmax23/Containerfiles

commit 909598723f21802a48279b69c19eecc78ffa5f51

Ensure images are up to date
The ansible tests run `gotmax23.update` before running tests so make
sure the images are up to date at buildtime to make that quicker.

Containerfiles/systemd/CentOS/CentOS.7.Containerfile
@@ -7,7 +7,8 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && yum update -y && yum -y --setopt=tsflags=nodoc install \
+    && yum upgrade -y --setopt=tsflags=nodoc && yum install -y --setopt=tsflags=nodoc
+ \
         systemd python2 sudo ca-certificates \
     && echo "**** Cleaning package cache ****" \
     && yum clean all \

Containerfiles/systemd/Debian/Debian.bookworm.Containerfile
@@ -8,7 +8,8 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get install -y --no-install-recommends \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
+ \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Debian/Debian.bullseye.Containerfile
@@ -8,7 +8,8 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get install -y --no-install-recommends \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
+ \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Debian/Debian.buster.Containerfile
@@ -8,7 +8,8 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get install -y --no-install-recommends \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
+ \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Ubuntu/Ubuntu.bionic.Containerfile
@@ -8,7 +8,8 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get install -y --no-install-recommends \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
+ \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Ubuntu/Ubuntu.focal.Containerfile
@@ -8,7 +8,8 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get install -y --no-install-recommends \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
+ \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Ubuntu/Ubuntu.jammy.Containerfile
@@ -8,7 +8,8 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get install -y --no-install-recommends \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
+ \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/AlmaLinux/AlmaLinux.8.Containerfile
@@ -7,7 +7,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && dnf -y --nodoc install \
+    && dnf upgrade -y --nodoc && dnf install -y --nodoc \
         systemd python3 sudo ca-certificates \
     && echo "**** Cleaning package cache ****" \
     && dnf clean all \

commit ed4532d6336166273176cb5444be2bb9192690fd

Remove spurious newlines
Fixes: 909598723f21802a48279b69c19eecc78ffa5f51

Containerfiles/systemd/CentOS/CentOS.7.Containerfile
@@ -7,8 +7,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && yum upgrade -y --setopt=tsflags=nodoc && yum install -y --setopt=tsflags=nodoc
- \
+    && yum upgrade -y --setopt=tsflags=nodoc && yum install -y --setopt=tsflags=nodoc \
         systemd python2 sudo ca-certificates \
     && echo "**** Cleaning package cache ****" \
     && yum clean all \

Containerfiles/systemd/Debian/Debian.bookworm.Containerfile
@@ -8,8 +8,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
- \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Debian/Debian.bullseye.Containerfile
@@ -8,8 +8,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
- \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Debian/Debian.buster.Containerfile
@@ -8,8 +8,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
- \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Ubuntu/Ubuntu.bionic.Containerfile
@@ -8,8 +8,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
- \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Ubuntu/Ubuntu.focal.Containerfile
@@ -8,8 +8,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
- \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

Containerfiles/systemd/Ubuntu/Ubuntu.jammy.Containerfile
@@ -8,8 +8,7 @@ LABEL maintainer="Maxwell G <gotmax23@github>"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends
- \
+    && apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y --no-install-recommends \
         systemd python3 sudo ca-certificates systemd-sysv python3-apt \
     && echo "**** Cleaning package cache ****" \
     && apt-get clean \

src/systemd/Containerfile.j2
@@ -9,7 +9,7 @@ ENV DEBIAN_FRONTEND=noninteractive
 {% endif %}
 
 RUN echo "**** Installing packages and updating if necessary ****" \
-    && {{ PACKAGE_MGR_INSTALL_CMD }} \
+    && {{ PACKAGE_MGR_INSTALL_CMD.rstrip() }} \
         {{ all_packages | join(' ') }} \
     && echo "**** Cleaning package cache ****" \
     && {{ PACKAGE_MGR_CLEAN_CMD }} \